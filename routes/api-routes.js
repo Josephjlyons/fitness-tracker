b0a05ede37d902808e8ce3ad38b3fcbc09a93c2d
Joe Lyons <josephjlyons90@gmail.com>
2021-04-15 17:20:42 -0400

    add async and await to first get

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,14 +2,16 @@ const router = require('express').Router();
 const Workout = require('../models/workout');
 
 
-router.get("/api/workouts", (req, res) => {
-    Workout.find({})
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
+router.get("/api/workouts", async (req, res) => {
+    try {
+
+        const workout = await Workout.find({})
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+    } catch (err) {
+        res.status(400).json(err);
+    };
 });
 
 
